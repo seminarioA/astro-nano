53b4b39890902e5a9c9b9787ce25f9b501847eda
ALEJANDRO VALENTINO SEMINARIO MEDINA <88020645+seminarioA@users.noreply.github.com>
2025-03-31 11:33:58 -0500

    modZeroWork

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   NAME: "myBlog",
   EMAIL: "markhorn.dev@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 4,
-  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_WORKS_ON_HOMEPAGE: 0,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 

3cb64d96eccf19f05eb1ec9d2676b4b79e9ef3e4
ALEJANDRO VALENTINO SEMINARIO MEDINA <88020645+seminarioA@users.noreply.github.com>
2025-03-31 11:32:01 -0500

    ModsocialMedia

--- src/consts.ts
+++ src/consts.ts
@@ -29,16 +29,18 @@ export const PROJECTS: Metadata = {
 };
 
 export const SOCIALS: Socials = [
+  /*
   { 
     NAME: "twitter-x",
     HREF: "https://twitter.com/markhorn_dev",
   },
+  */
   { 
     NAME: "github",
     HREF: "https://github.com/markhorn-dev"
   },
   { 
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    HREF: "https://www.linkedin.com/in/alejandroseminariomedina/",
   }
 ];
